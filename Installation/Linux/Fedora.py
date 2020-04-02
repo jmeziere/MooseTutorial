f9ef445f2dccdc3526b8c99618e7660579d269a5
jmeziere <mez16003@byui.edu>
2020-04-02 17:17:12 -0600

    Fix small errors in installation

--- Installation/Linux/Fedora.py
+++ Installation/Linux/Fedora.py
@@ -2,28 +2,29 @@ from kivy.uix.screenmanager import Screen
 import os
 import subprocess
 from kivy.lang import Builder
+import _thread
 
 class Fedora(Screen):
     def install(self):
-        os.system("sudo dnf update; sudo -E dnf install -y cc gcc-c++ gcc-gfortran tcl tk findutils make freeglut-devel libXt-devel libX11-devel m4 blas-devel lapack-devel git xz-devel; sudo dnf install -y wget; cd ..; mkdir install; cd install; wget -c www.mooseframework.org/moose_packages/moose-environment_fedora-31_20191125_x86_64.rpm?_ga=2.109505677.9080527.1585322568-1085612855.1582900245; sudo rpm -i moose-environment_fedora-31_20191125_x86_64.rpm?_ga=2.109505677.9080527.1585322568-1085612855.1582900245'")
+        os.system("sudo dnf update; sudo -E dnf install -y cc gcc-c++ gcc-gfortran tcl tk findutils make freeglut-devel libXt-devel libX11-devel m4 blas-devel lapack-devel git xz-devel; sudo dnf install -y wget; cd ..; mkdir install; cd install; wget -c www.mooseframework.org/moose_packages/moose-environment_fedora-31_20191125_x86_64.rpm?_ga=2.109505677.9080527.1585322568-1085612855.1582900245; sudo rpm -i moose-environment_fedora-31_20191125_x86_64.rpm?_ga=2.109505677.9080527.1585322568-1085612855.1582900245")
 
     def install_too(self):
-        os.system("sudo module load moose-dev-gcc'")
+        os.system("sudo module load moose-dev-gcc")
 
         try:
             subprocess.check_output("grep 'module load moose-dev-gcc' ~/.bashrc", shell = True)
         except subprocess.CalledProcessError as e:
-            os.system("sudo echo \"module load moose-dev-gcc\" >> ~/.bashrc'")
+            os.system("sudo echo \"module load moose-dev-gcc\" >> ~/.bashrc")
 
         os.system("mkdir ~/projects; cd ~/projects; git clone https://github.com/idaholab/moose.git; cd moose; git checkout master")
 
         os.system("sudo rm -r moosepath.txt")
-        os.system("sudo echo \""+moosepath+"\" > "+os.path.join(os.path.dirname(__file__),"moosepath.txt"))
+        os.system("sudo echo \"~\" > "+os.path.join(os.path.dirname(__file__),"moosepath.txt"))
 
         try:
             subprocess.check_output("grep 'export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH' ~/.bashrc", shell = True)
         except subprocess.CalledProcessError as e:
-            os.system("sudo echo \"export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH\" >> ~/.bashrc'")
+            os.system("sudo echo \"export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH\" >> ~/.bashrc")
 
         os.system("cd ~/projects/moose; ./scripts/update_and_rebuild_libmesh.sh")
 
--- Installation/Linux/Ubuntu18_04.py
+++ Installation/Linux/Ubuntu18_04.py
@@ -6,25 +6,25 @@ import _thread
 
 class Ubuntu18_04(Screen):
     def install(self):
-        os.system("sudo apt-get update; sudo apt-get install -y build-essential gfortran tcl git m4 freeglut3 doxygen libblas-dev liblapack-dev libx11-dev libnuma-dev libcurl4-gnutls-dev zlib1g-dev libhwloc-dev libxml2-dev libpng-dev pkg-config liblzma-dev; sudo apt install -y wget; cd ..; mkdir install; cd install; wget -c www.mooseframework.org/moose_packages/moose-environment_ubuntu-18.04_20191125_x86_64.deb?_ga=2.140379123.1980279153.1583519867-1085612855.1582900245; sudo dpkg -i moose-environment_ubuntu-18.04_20191125_x86_64.deb?_ga=2.140379123.1980279153.1583519867-1085612855.1582900245'")
+        os.system("sudo apt-get update; sudo apt-get install -y build-essential gfortran tcl git m4 freeglut3 doxygen libblas-dev liblapack-dev libx11-dev libnuma-dev libcurl4-gnutls-dev zlib1g-dev libhwloc-dev libxml2-dev libpng-dev pkg-config liblzma-dev; sudo apt install -y wget; cd ..; mkdir install; cd install; wget -c www.mooseframework.org/moose_packages/moose-environment_ubuntu-18.04_20191125_x86_64.deb?_ga=2.140379123.1980279153.1583519867-1085612855.1582900245; sudo dpkg -i moose-environment_ubuntu-18.04_20191125_x86_64.deb?_ga=2.140379123.1980279153.1583519867-1085612855.1582900245")
 
     def install_too(self):
-        os.system("sudo module load moose-dev-gcc'")
+        os.system("sudo module load moose-dev-gcc")
 
         try:
             subprocess.check_output("grep 'module load moose-dev-gcc' ~/.bashrc", shell = True)
         except subprocess.CalledProcessError as e:
-            os.system("sudo echo \"module load moose-dev-gcc\" >> ~/.bashrc'")
+            os.system("sudo echo \"module load moose-dev-gcc\" >> ~/.bashrc")
 
         os.system("mkdir ~/projects; cd ~/projects; git clone https://github.com/idaholab/moose.git; cd moose; git checkout master")
 
         os.system("sudo rm -r moosepath.txt")
-        os.system("sudo echo \""+moosepath+"\" > "+os.path.join(os.path.dirname(__file__),"moosepath.txt"))
+        os.system("sudo echo \"~\" > "+os.path.join(os.path.dirname(__file__),"moosepath.txt"))
 
         try:
             subprocess.check_output("grep 'export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH' ~/.bashrc", shell = True)
         except subprocess.CalledProcessError as e:
-            os.system("sudo echo \"export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH\" >> ~/.bashrc'")
+            os.system("sudo echo \"export PATH="+os.path.join(commonMethods.moosepath,projects/moose/python/peacock)+":$PATH\" >> ~/.bashrc")
 
         os.system("cd ~/projects/moose; ./scripts/update_and_rebuild_libmesh.sh")
 
